d350367ebddc1c7a6b201f63283ecec22466fcdb
Cory Forsstrom <cforsstrom18@gmail.com>
2020-02-27 15:44:56 -0800

    move hosts update to entrypoint

--- run.sh
+++ run.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-export IP=$(getent ahostsv4 freegamez.ga | awk '{ print $1 }' | head -1)
-
 docker run -d \
-	  --name=xteve_lazystream \
-	  --log-opt max-size=10m \
-	  --log-opt max-file=3 \
-	  -e TZ="America/Los_Angeles" \
-	  -v /mnt/user/appdata/xteve/:/root/.xteve:rw \
-	  -v /mnt/user/appdata/xteve/_config/:/config:rw \
-	  -v /mnt/user/appdata/xteve/_guide2go/:/guide2go:rw \
-          -v /mnt/user/appdata/xteve/playlists/:/playlists:rw \
-	  -v /tmp/xteve/:/tmp/xteve:rw \
-          --add-host="mf.svc.nhl.com:$IP" \
-	  --add-host="playback.svcs.mlb.com:$IP" \
-	  --add-host="mlb-ws-mf.media.mlb.com:$IP" \
-	  taylorbourne/xteve_lazystream
+	--name=xteve_lazystream \
+	--log-opt max-size=10m \
+	--log-opt max-file=3 \
+	-e TZ="America/Los_Angeles" \
+	-v /mnt/user/appdata/xteve/:/root/.xteve:rw \
+	-v /mnt/user/appdata/xteve/_config/:/config:rw \
+	-v /mnt/user/appdata/xteve/_guide2go/:/guide2go:rw \
+	-v /mnt/user/appdata/xteve/playlists/:/playlists:rw \
+	-v /tmp/xteve/:/tmp/xteve:rw \
+	taylorbourne/xteve_lazystream
