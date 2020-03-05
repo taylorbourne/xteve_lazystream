3e57bca56e20f9172adc74c6c8aafebb7478de26
Cory Forsstrom <cforsstrom18@gmail.com>
2020-03-05 10:06:20 -0800

    change mappings so all under one folder

--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@ docker run -d \
 	--log-opt max-size=10m \
 	--log-opt max-file=3 \
 	-e TZ="America/Los_Angeles" \
-	-v /mnt/user/appdata/xteve/:/xteve:rw \
-	-v /mnt/user/appdata/config/:/config:rw \
-	-v /mnt/user/appdata/guide2go/:/guide2go:rw \
-	-v /mnt/user/appdata/playlists/:/playlists:rw \
+	-v /mnt/user/appdata/xteve/.xteve:/xteve:rw \
+	-v /mnt/user/appdata/xteve/config/:/config:rw \
+	-v /mnt/user/appdata/xteve/guide2go/:/guide2go:rw \
+	-v /mnt/user/appdata/xteve/playlists/:/playlists:rw \
 	-v /tmp/xteve/:/tmp/xteve:rw \
 	taylorbourne/xteve_lazystream
